fafb5ac115257bbd9dca9feee6857316572f01a6
Jayesh Sanghvi <jayeshsanghvi@Jayesh.local>
2026-02-09 11:03:58 +0530

    Fix Render port binding

--- app.py
+++ app.py
@@ -138,5 +138,11 @@ def search():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
 
